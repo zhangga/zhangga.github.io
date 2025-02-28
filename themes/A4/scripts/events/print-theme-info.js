1b9d918ff24c1c669898609c2fefc70acbda74ff
ZhangZeQiang <383523842@qq.com>
2025-02-28 14:25:09 +0800

    add theme/a4

---
+++ themes/A4/scripts/events/print-theme-info.js
@@ -0,0 +1,21 @@
+/* global hexo */
+
+'use strict'
+
+hexo.on('ready', () => {
+  if (!/^(g|s)/.test(hexo.env.cmd)) return
+  const { version } = require('../../package.json')
+  hexo.log.info(`
+
+Welcome.
+----------------------------------------------------------------
+   ___  ____
+  / _ |/ / /
+ / __ /_  _/
+/_/ |_|/_/                                 Reading and writing.
+-----------------------------------------------------------------
+Your hexo-theme-A4 version is ${version}. Maybe it can be updated.
+The latest: https://github.com/HiNinoJay/hexo-theme-A4/releases
+-----------------------------------------------------------------
+`)
+})
